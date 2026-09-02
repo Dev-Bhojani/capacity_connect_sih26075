112830e6f50e6697d6e985e2fc19df9a64eeeec6
Dev-Bhojani <bhojanidev5@gmail.com>
2026-09-02 19:41:46 +0530

    initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import "./App.css";
+import Card from "./Reusable_components/Card/Card";
+import Textarea from "./Reusable_components/Textarea/Textarea";
+import SearchBar from "./Reusable_components/SearchBar/SearchBar";
+import ErrorState from "./Reusable_components/ErrorState/ErrorState";
+import Dropdown from "./Reusable_components/Dropdown/Dropdown";
+import {
+  FiBarChart2,
+  FiChevronDown,
+  FiDownload,
+  FiTrash2,
+} from "react-icons/fi";
+import Button from "./Reusable_components/Button/Button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+     
+    </>
+  );
+}
+
+export default App;
